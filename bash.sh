332a24d9ea4836c86804c77a6e4ad8245222f00e
Jamie Git Git <justicehopelove@yahoo.com>
2019-02-08 16:11:19 -0800

    split about me and gallery pages

--- bash.sh
+++ bash.sh
@@ -1,1 +1,5 @@
 cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+
+cat templates/top.html templates/topheader.html content/about-me.html templates/bottomfooter.html templates/bottom.html > docs/about-me.html
+
+cat templates/top.html templates/topheader.html content/gallery.html templates/bottomfooter.html templates/bottom.html > docs/gallery.html
