f50678bfc72e009042cc7461e90410226bfaf5ba
Jamie Git Git <justicehopelove@yahoo.com>
2019-02-08 16:14:21 -0800

    split contribute page

--- bash.sh
+++ bash.sh
@@ -3,3 +3,5 @@ cat templates/top.html content/index.html templates/bottom.html > docs/index.htm
 cat templates/top.html templates/topheader.html content/about-me.html templates/bottomfooter.html templates/bottom.html > docs/about-me.html
 
 cat templates/top.html templates/topheader.html content/gallery.html templates/bottomfooter.html templates/bottom.html > docs/gallery.html
+
+cat templates/top.html templates/topheader.html content/contribute.html templates/bottomfooter.html templates/bottom.html > docs/contribute.html
