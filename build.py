e40310956b32ce0fc15a6b3913b9e1c24acf600e
Jamie Git Git <justicehopelove@yahoo.com>
2019-02-09 20:07:35 -0800

    about me page

--- build.py
+++ build.py
@@ -8,13 +8,13 @@ bottom_footer = open('templates/bottomfooter.html').read()
 content = open('content/index.html').read()
 
 # Combine template HTML code with content HTML code
-#index_html = top_template + content + bottom_template
-#open('index.html', 'w+').write(index_html)
+index_html = top_template + content + bottom_template
+open('docs/index.html', 'w+').write(index_html)
 
-# Rinse and repeat, but with blog HTML code
-#content = open('middle_blog.html').read()
-#blog_html = top_template + content + bottom_template
-#open('blog.html', 'w+').write(blog_html)
+# Rinse and repeat, but with about me HTML code
+content = open('content/about-me.html').read()
+aboutme_html = top_template + top_header + content + bottom_footer + bottom_template
+open('docs/about-me.html', 'w+').write(aboutme_html)
 
 # And again, this time with art HTML code
 #content = open('middle_art.html').read()
